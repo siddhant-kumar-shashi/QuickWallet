78cb29cd6559f49a22d657bc3d1dc3c998654a16
siddhant <siddhant2479as@gmail.com>
2025-08-29 16:05:39 +0530

    commited

--- apps/user-app/app/(dashboard)/transactions/page.tsx
+++ apps/user-app/app/(dashboard)/transactions/page.tsx
@@ -1,6 +1,31 @@
+import prisma from "@repo/db/client";
+import { AddMoney } from "../../../components/AddMoneyCard";
+import { BalanceCard } from "../../../components/BalanceCard";
+import { OnRampTransactions } from "../../../components/OnRampTransactions";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../lib/auth";
+import { redirect } from 'next/navigation'
 
-export default function() {
-    return <div>
+async function getOnRampTransactions() {
+    const session = await getServerSession(authOptions);
+    const txns = await prisma.onRampTransaction.findMany({
+        where: {
+            userId: Number(session?.user?.id)
+        }
+    });
+    return txns.map(t => ({
+        time: t.startTime,
+        amount: t.amount,
+        status: t.status,
+        provider: t.provider
+    }))
+}
+
+export default async function() {
+     const transactions = await getOnRampTransactions();
+
+    return <div className="pt-4 w-full">
         sidTransactions
+        <OnRampTransactions transactions={transactions} />
     </div>
 }
